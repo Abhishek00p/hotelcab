37834fc8e7f8e3f4ca48c83b7978798fb9e76e3c
ABHISHEK PRAJAPAT <prajapatabhishek9326@gmail.com>
2022-02-01 14:35:05 +0530

    new changes

--- lib/home.dart
+++ lib/home.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hotelcab/main.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
+  runApp(const MaterialApp(
+    home:  HomePage(),
     title: "its home page",
   ));
 }
@@ -14,13 +14,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-      home: Column(
-        children: [
-          TextButton.icon(onPressed: ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> const Hotel())), icon: const Icon(Icons.home),label: const Text("data"),),
-        ],
-      )
-      
-    );
+        home: Column(
+      children: [
+        TextButton.icon(
+          onPressed: () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) =>  Hotel())),
+          icon: const Icon(Icons.home),
+          label: const Text(""),
+        ),
+      ],
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:hotelcab/home.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Hotel(),
   ));
 }
 
 class Hotel extends StatelessWidget {
-  const Hotel({Key? key}) : super(key: key);
+  Hotel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +21,65 @@ class Hotel extends StatelessWidget {
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const HomePage())),
               icon: const Icon(Icons.search),
-              label: const Text("fg"))
+              label: const Text(""))
         ],
       ),
-      body: const Text("hello"),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 10.0,
+              ),
+              const Text("Number of passanger "),
+              Container(
+                  width: 300.0,
+                  child: ListView(
+                    padding: EdgeInsets.all(8.0),
+                    children: <Widget>[
+                      Container(
+                        height: 30.0,
+                        child: Text("dd"),
+                      ),
+                      Container(
+                        height: 30.0,
+                        child: Text("hh"),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 10.0,
+              ),
+              const Text("Customer Govt ID "),
+              Container(
+                  width: 200.0,
+                  child: TextField(
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      height: 0.8,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: " + Insert",
+                      fillColor: Colors.grey,
+                      hoverColor: Colors.amber,
+                      filled: true,
+                    ),
+                    keyboardType: TextInputType.number,
+                  ))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
