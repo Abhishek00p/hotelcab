5282067438c801c3d25a94dc24c44a37818ec9db
ABHISHEK PRAJAPAT <prajapatabhishek9326@gmail.com>
2022-02-10 15:22:01 +0530

    .

--- lib/home.dart
+++ lib/home.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:hotelcab/main.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home:  HomePage(),
-    title: "its home page",
-  ));
-}
-
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Column(
+    return Scaffold(
+        body: Container(
+            child: Column(
       children: [
+        SizedBox(
+          height: 40.0,
+        ),
         TextButton.icon(
           onPressed: () => Navigator.push(
-              context, MaterialPageRoute(builder: (context) =>  Hotel())),
+              context, MaterialPageRoute(builder: (context) => Hotel())),
           icon: const Icon(Icons.home),
           label: const Text(""),
         ),
       ],
-    ));
+    )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:hotelcab/home.dart';
+import 'home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Hotel(),
-  ));
-}
+void main() => runApp(MaterialApp(home: Hotel()));
 
 class Hotel extends StatelessWidget {
-  Hotel({Key? key}) : super(key: key);
+  const Hotel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         backgroundColor: Colors.black,
+        foregroundColor: Colors.white70,
         title: const Text("HotelCab"),
         actions: [
-          TextButton.icon(
-              onPressed: () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const HomePage())),
-              icon: const Icon(Icons.search),
-              label: const Text(""))
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomePage()));
+            },
+            icon: const Icon(Icons.search),
+          ),
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 10.0,
-              ),
-              const Text("Number of passanger "),
-              Container(
-                  width: 300.0,
-                  child: ListView(
-                    padding: EdgeInsets.all(8.0),
-                    children: <Widget>[
-                      Container(
-                        height: 30.0,
-                        child: Text("dd"),
-                      ),
-                      Container(
-                        height: 30.0,
-                        child: Text("hh"),
-                      ),
-                    ],
-                  ))
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            children: [
-              const SizedBox(
-                width: 10.0,
-              ),
-              const Text("Customer Govt ID "),
-              Container(
-                  width: 200.0,
-                  child: TextField(
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      height: 0.8,
-                    ),
-                    decoration: InputDecoration(
-                      hintText: " + Insert",
-                      fillColor: Colors.grey,
-                      hoverColor: Colors.amber,
-                      filled: true,
-                    ),
-                    keyboardType: TextInputType.number,
-                  ))
-            ],
-          )
-        ],
-      ),
+      body: Column(children: [
+        SizedBox(
+          height: 10.0,
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 20.0,
+            ),
+            Text("Number of passanger  "),
+          ],
+        )
+      ]),
     );
   }
 }
